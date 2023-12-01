a5b111777a0d53f66256be9f4f0fc64648000375
ungstrup <8708181+ungstrup@users.noreply.github.com>
2023-12-01 16:24:48 +0100

    Add custom font, work on color scheme and layout

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const newBookBtn = document.querySelector('.new-book-btn');
 const openDialog = document.querySelector('.new-book-dialog');
 const addBookBtn = document.querySelector('.add-btn');
 const closeDialogBtn = document.querySelector('.close-btn');
+const inputBoxes = document.querySelectorAll('input');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -25,18 +26,18 @@ function updateLibrary() {
         newBook.dataset.book = myLibrary.indexOf(book);
         libraryContainer.appendChild(newBook);
         let bookContent = document.createElement('p');
-        bookContent.textContent=`Title: ${book.title}\r\nAuthor: ${book.author}\r\nPages: ${book.pages}\r\n${book.read ? "You have read this book" : "You have not read this book yet"}`;
+        bookContent.textContent=`Title: ${book.title}\r\nAuthor: ${book.author}\r\nPages: ${book.pages}\r\n Status: ${book.read ? "Read" : "Not read yet"}`;
         newBook.appendChild(bookContent);
-        let removeButton = document.createElement('button');
-        removeButton.classList.add('remove-btn');
-        removeButton.textContent = 'Remove Book';
-        newBook.appendChild(removeButton);
-        removeButton.addEventListener('click', (e) => removeBook(e))
         let readButton = document.createElement('button');
         readButton.classList.add('read-btn');
         readButton.textContent = book.read ? 'Not read' : 'Read';
         readButton.addEventListener('click', (e) => readUpdate(e));
         newBook.appendChild(readButton);
+        let removeButton = document.createElement('button');
+        removeButton.classList.add('remove-btn');
+        removeButton.textContent = 'Remove Book';
+        newBook.appendChild(removeButton);
+        removeButton.addEventListener('click', (e) => removeBook(e))
 });
 };
 
@@ -49,7 +50,6 @@ function readUpdate(book) {
 
 function removeBook(book) {
     let index = book.target.parentNode.dataset.book;
-    libraryContainer.removeChild(book.target.parentNode)
     myLibrary.splice(index, 1);
     updateLibrary();
 };
@@ -60,6 +60,7 @@ function addBook(title, author, pages, read) {
 };
 
 newBookBtn.addEventListener('click', () => {
+    clearInput();
     openDialog.showModal();
 });
 
@@ -67,10 +68,16 @@ closeDialogBtn.addEventListener('click', () => {
     openDialog.close();
 });
 
+function clearInput() {
+    inputBoxes.forEach(input => {
+        input.value = "";
+    });
+};
+
 addBookBtn.addEventListener('click', () => {
     let title = document.getElementById('book-title').value;
     let author = document.getElementById('book-author').value;
-    let pages = document.getElementById('book-pages').value;
+    let pages = parseInt(document.getElementById('book-pages').value);
     let read = (document.getElementById('book-isread').checked) ? true : false;
     addBook(title, author, pages, read);
 });
